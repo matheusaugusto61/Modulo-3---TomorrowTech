f1aa5ac02a805c6912ef137adf936e1934d71bf1
matheusaugusto61 <matheussouza.1802@aluno.unibh.br>
2022-05-25 21:41:32 -0300

    corrigindo main

--- src/exercicio1/Main.java
+++ src/exercicio1/Main.java
@@ -9,11 +9,15 @@ public class Main {
     public static void main(String[] args) {
         List<Double> list = new ArrayList<>();
         list.add(5d);
-        list.add(0d);
+        list.add(2d);
 
         Calculo subtracao = new Subtracao();
         System.out.println(subtracao.calculo(list));
         Calculo divisao = new Divisao();
         System.out.println(divisao.calculo(list));
+        Calculo multiplicacao = new Multiplicacao();
+        System.out.println(multiplicacao.calculo(list));
+        Calculo soma = new Soma();
+        System.out.println(soma.calculo(list));
     }
 }
